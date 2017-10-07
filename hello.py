26b8fd104ccf3d74e7424625e639bf31cf9fd63a
wongchunhung <chunha@gmail.com>
2017-10-07 14:17:01 +0800

    edit input

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hi')
+@app.route('/helloworld')
 def hello_world():
-	return 'Hello World2!'
+	return 'Hello Hello World 2!'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
